456a2f264ce38d8254b2360fbbe727a329b8fed8
drblack001 <21rajatsharma98@gmail.com>
2026-07-28 10:12:21 +0530

    xyz

--- indes.js
+++ indes.js
@@ -1,1 +1,52 @@
-console.log("Hell0");
+// // console.log("Hell0");
+
+// const sum = function(a,b){
+//     // return (a**(1/2))+(b**(1/2));
+//     return Math.sqrt(a)+Math.sqrt(b);
+// }
+// console.log(sum(4,9))
+
+// const sum =(a,b)=>{return a+b}
+// console.log(sum(1111,889))
+
+//  IIFE immediate invoke fcuntion as exp
+// (()=>{
+//     console.log("hiiiii")
+// })();
+
+// let a=23;
+// if(a>20){
+//     let a=40
+//     console.log("value inside the block "+a)
+// }
+// console.log(a)
+
+// call back function
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// function msgwithsum(clbk, msg){
+//     const result = clbk(40,50);
+//     console.log("hiiiiiiiiii "+ msg+" and result "+result)
+// }
+// msgwithsum(sum,"abc");
+
+function login(error,msg){
+    if(error){
+        console.log("Error is "+error)
+    }
+    else{
+        console.log(msg)
+    }
+}
+function loginhandler(username , pass, clbk){
+    if(username == "drblack001" && pass == "12345"){
+        clbk(null,'login success')
+    }
+    else{
+        clbk("useranme or password incorrect", null)
+    }
+}
+console.log(loginhandler("drblack001", 12345,login))
