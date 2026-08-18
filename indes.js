6dde1428dd533487e884af828b5d750cac43ab56
drblack001 <21rajatsharma98@gmail.com>
2026-08-18 09:14:25 +0530

    index updated

--- indes.js
+++ indes.js
@@ -1,52 +1,69 @@
-// // console.log("Hell0");
+// // // console.log("Hell0");
 
-// const sum = function(a,b){
-//     // return (a**(1/2))+(b**(1/2));
-//     return Math.sqrt(a)+Math.sqrt(b);
+// // const sum = function(a,b){
+// //     // return (a**(1/2))+(b**(1/2));
+// //     return Math.sqrt(a)+Math.sqrt(b);
+// // }
+// // console.log(sum(4,9))
+
+// // const sum =(a,b)=>{return a+b}
+// // console.log(sum(1111,889))
+
+// //  IIFE immediate invoke fcuntion as exp
+// // (()=>{
+// //     console.log("hiiiii")
+// // })();
+
+// // let a=23;
+// // if(a>20){
+// //     let a=40
+// //     console.log("value inside the block "+a)
+// // }
+// // console.log(a)
+
+// // call back function
+
+// // function sum(a,b){
+// //     return a+b;
+// // }
+
+// // function msgwithsum(clbk, msg){
+// //     const result = clbk(40,50);
+// //     console.log("hiiiiiiiiii "+ msg+" and result "+result)
+// // }
+// // msgwithsum(sum,"abc");
+
+// function login(error,msg){
+//     if(error){
+//         console.log("Error is "+error)
+//     }
+//     else{
+//         console.log(msg)
+//     }
 // }
-// console.log(sum(4,9))
-
-// const sum =(a,b)=>{return a+b}
-// console.log(sum(1111,889))
-
-//  IIFE immediate invoke fcuntion as exp
-// (()=>{
-//     console.log("hiiiii")
-// })();
-
-// let a=23;
-// if(a>20){
-//     let a=40
-//     console.log("value inside the block "+a)
+// function loginhandler(username , pass, clbk){
+//     if(username == "drblack001" && pass == "12345"){
+//         clbk(null,'login success')
+//     }
+//     else{
+//         clbk("useranme or password incorrect", null)
+//     }
 // }
-// console.log(a)
+// console.log(loginhandler("drblack001", 12345,login))
 
-// call back function
+// // syn funciton
+// console.log("Hi");
+// console.log("Geek");
+// console.log("How are you?");
 
-// function sum(a,b){
-//     return a+b;
-// }
 
-// function msgwithsum(clbk, msg){
-//     const result = clbk(40,50);
-//     console.log("hiiiiiiiiii "+ msg+" and result "+result)
-// }
-// msgwithsum(sum,"abc");
+// //asyn fcuntion
+// console.log(1)
+// setTimeout(()=>{console.log("2")},0.0000000)
+// console.log(3)
 
-function login(error,msg){
-    if(error){
-        console.log("Error is "+error)
-    }
-    else{
-        console.log(msg)
-    }
+console.log("1")
+for( i=0;i<1000;i++){
+    console.log("i="+i);
 }
-function loginhandler(username , pass, clbk){
-    if(username == "drblack001" && pass == "12345"){
-        clbk(null,'login success')
-    }
-    else{
-        clbk("useranme or password incorrect", null)
-    }
-}
-console.log(loginhandler("drblack001", 12345,login))
+console.log("3")
