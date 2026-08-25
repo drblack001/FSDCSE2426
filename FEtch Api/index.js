e7c9b27bd31acba2ce72f45f4f33ef1af1d331c2
drblack001 <21rajatsharma98@gmail.com>
2026-08-25 10:10:44 +0530

    2nd Commit

---
+++ FEtch Api/index.js
@@ -0,0 +1,10 @@
+// console.log("helo");
+
+const root = document.getElementById('container');
+const button = document.getElementById('btn')
+
+
+function fetch(){
+    alert('hiii')
+}
+button.addEventListener('click', fetch)
