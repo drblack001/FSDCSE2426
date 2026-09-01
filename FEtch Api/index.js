543b6d8d9f551719b0c4f9d2a651961508bf26ba
drblack001 <21rajatsharma98@gmail.com>
2026-09-01 10:01:52 +0530

    fetch api

--- FEtch Api/index.js
+++ FEtch Api/index.js
@@ -1,10 +1,45 @@
-// console.log("helo");
+// console.log("hiii")
+
 
 const root = document.getElementById('container');
-const button = document.getElementById('btn')
+const button = document.getElementById('btn');
+const disp = document.getElementById('text1');
+const loader = document.getElementById('loader');
 
+async function fetchData() {
 
-function fetch(){
-    alert('hiii')
+    try {
+        loader.innerHTML='<h2 style=color:red> Loading data.....</h2>'
+        const serverData = await fetch('https://fakestoreapi.com/products');
+        const jsonData = await serverData.json()
+
+        // disp.innerHTML=`${jsonData[1].title}`
+
+        // console.log(jsonData[1].title);
+
+        let table=`<table  border=2px>
+        ${
+            jsonData.map((ele)=>
+                `<tr>
+                    <td> <img src=${ele.image} height=200 width=200> </img> </td>
+                    <td> ${ele.title}  </td>
+                    <td> ${ele.description} </td>
+                    <td> ${ele.price} </td>
+                    <td> ${ele.category} </td>
+                </tr>`
+            )
+        }
+        </table>`
+
+        disp.innerHTML=table;
+
+    } catch (e) {
+        console.log("Error is" + e);
+    }
+    finally {
+        loader.innerHTML='';
+    }
+
 }
-button.addEventListener('click', fetch)
+
+button.addEventListener('click', fetchData);
